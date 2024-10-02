04f8531d922a0679bd3abea0fef8c733aaf5fc0e
Joao Paulo Reis Marques <joaopaulo.rm7@gmail.com>
2024-10-02 10:24:53 -0400

    refactor for groq

---
+++ config.py
@@ -0,0 +1,3 @@
+import os
+
+APIKEY = os.environ.get("GROQ_API_KEY")
--- transcription_module.py
+++ transcription_module.py
@@ -1,23 +1,24 @@
-from openai import OpenAI, APIError
-from config import APIKEY
+from time import sleep
+from groq import Groq
 import os
-import similarity_folders 
+from config import APIKEY
+import similarity_folders
+import traceback
 
-# OpenAI API KEY
-client = OpenAI(api_key=APIKEY)
+# Groq API KEY
+client = Groq(api_key=APIKEY)
 
 index = 0
 
 def read_text_file(file_path):
-    with open(file_path, 'r') as file:
-        text = file.read()
-        # Remove extra white spaces
-        text = text.strip()
-        # Remove blank lines
-        text = '\n'.join(line for line in text.splitlines() if line.strip())
-        return text
+    try:
+        with open(file_path, 'r') as file:
+            text = file.read().strip()
+            return text if text else ""  # Retornar string vazia se o texto estiver vazio
+    except Exception as e:
+        print(f"Error reading file {file_path}: {e}")
+        return ""
 
-# Function to write into the file
 def write_text_file(file_path, text):
     with open(file_path, 'w') as file:
         file.write(text)
@@ -26,60 +27,54 @@ def the_world_is_our(input_folder, output_folder):
     global index
     print(len(os.listdir(input_folder)))
     
-    # Identify and discard similar documents
     similar_documents, documents_to_discard = similarity_folders.compare_documents_in_folder(input_folder)
 
-    # List the remaining documents after discarding similar ones
     list_docs = similarity_folders.list_arquivos(input_folder, documents_to_discard)
-    # Print the number of documents after similarity check
     print(len(list_docs))
-    # Create the output folder if it doesn't exist
     os.makedirs(output_folder, exist_ok=True)
     print("=======The program is running=======")
 
     while index < len(list_docs):
         try:
             for i in list_docs[index:]:
-                # Extract the base name of the file
                 tc_name = os.path.basename(i)
-                # Read the content of the current document
                 input_text = read_text_file(i)
+                
+                if input_text is None or input_text == "":
+                    print(f"Warning: The input file {i} is empty or not found.")
+                    continue  # Pular este arquivo se estiver vazio
 
-                # Generate a response using the OpenAI GPT-3.5 Turbo model
                 response = client.chat.completions.create(
-                    model="gpt-3.5-turbo-1106",
+                    model="llama3-8b-8192",
                     messages=[
-                        # Training the model using Few-Shot learning
                         {'role': 'user', 'content': read_text_file('files/Scripts/TC_.activity.account.AccountsActivity_20210411-144635.txt')},
                         {'role': 'assistant', 'content': read_text_file('files/Transcriptions/TC_.activity.account.AccountsActivity_20210411-144635.txt')},
                         {'role': 'user', 'content': read_text_file('files/Scripts/TC_.activity.account.TransferActivity_20210411-144754.txt')},
                         {'role': 'assistant', 'content': read_text_file('files/Transcriptions/TC_.activity.account.TransferActivity_20210411-144754.txt')},
                         {'role': 'user', 'content': input_text}
                     ],
-                    max_tokens=1100,
-                    n=1,
-                    temperature=0.5
+                    temperature=1,
+                    max_tokens=8192,
+                    top_p=1,
+                    stream=True,
+                    stop=None,
                 )
 
-                # Extract the generated output text
-                output_text = response.choices[0].message.content.strip()
-                # Construct the output file path and write the result to the file
+                output_text = ""
+                for part in response:
+                    delta_content = part.choices[0].delta.content
+                    if delta_content is not None:  # Verifique se o conteúdo não é None
+                        output_text += delta_content
+
                 output_file_path = os.path.join(output_folder, tc_name)
                 write_text_file(output_file_path, output_text)
-                # Print a message indicating that the result has been saved to the file
                 print("The result has been saved to the file", output_file_path)
 
-                index += 1  # Increment the value of index
-
-        except APIError as te:
-            if te.http_status == "502":
-                print(te)
-                last_doc = index + list_docs[index:].index(i)
-                print(f"Error occurred at index {last_doc} - {i}")
-                index = last_doc
+                index += 1
 
         except Exception as e:
-            print(e)
+            print("An error occurred:")
+            traceback.print_exc()  # Fornecer mais detalhes sobre o erro
             last_doc = index + list_docs[index:].index(i)
             print(f"Error occurred at index {last_doc} - {i}")
             index = last_doc + 1
